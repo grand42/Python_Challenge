24e15fb03ee9d94211833ffafc0371d56b90dfba
Gretchen A <and01487@umn.edu>
2020-10-09 09:43:41 -0500

    Set up csv reader and create lists

---
+++ PyPoll/main.py
@@ -0,0 +1,23 @@
+# Modules
+import os
+import csv
+
+# Set Path
+csvpath = os.path.join("Resources","budget_data.csv")
+
+# Define Lists
+Voter_ID = []
+County = []
+Candidate=[]
+
+# Open CSV
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header=next(csvreader)
+
+    for row in csvreader:
+        Voter_ID.append(row[0])
+        County.append(row[1])
+        Candidate.append(row[2])
+
