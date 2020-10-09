84b6816f1d220a977cff96448f56778c5ea53237
Gretchen A <and01487@umn.edu>
2020-10-09 14:46:12 -0500

    Calculate Winner of Election

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -21,8 +21,15 @@ with open(csvpath) as csvfile:
         County.append(row[1])
         Candidate.append(row[2])
 
+# Print Header
+print("Election Results")
+print("--------------------")
+
 # Find Total Votes
 total = len(Voter_ID)
+print(f"Total Votes: {total}")
+
+print("--------------------")
 
 # Find unique candides
 Candidate_List = []
@@ -30,12 +37,28 @@ for vote in Candidate:
     if vote not in Candidate_List:
         Candidate_List.append(vote)
 
+# Define function to find percentage of votes
+
 def vote_percent(candidate_vote):
     x = Candidate.count(candidate_vote)
     Total = len(Voter_ID)
+    percent = 0.00
     percent = (x/Total) * 100
-    print(candidate_vote + ": " + str(percent) + "% (" + str(x) + ")")
+    percent = str(round(percent, 3))
+    print(candidate_vote + ": " + percent + "% (" + str(x) + ")")
+
+# Create list for vote counts to find winner
+winner = []
 
 
 for candidate in Candidate_List:
     vote_percent(candidate)
+    
+    # Find Votes/Candidate
+    winner.append(int(Candidate.count(candidate)))
+
+#Find Winner
+winner_index = winner.index(max(winner))
+print("--------------------")
+print(f"Winner: {Candidate_List[winner_index]}")
+print("--------------------")
