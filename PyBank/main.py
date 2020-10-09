037af0698eb50f7333f64e129a9a39f8a5dffe02
Gretchen A <and01487@umn.edu>
2020-10-09 00:46:12 -0500

    Add Average

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,32 +3,39 @@ import os
 import csv
 
 # Set Path
-csvpath = os.path.join("..", "Resources","budget_data.csv")
+csvpath = os.path.join("Resources","budget_data.csv")
 
 # Define Lists
 date = []
 Profits_Losses = []
 
 # Define Function
-def profits_and_losses(budget_data):
+# def profits_and_losses(profits):
     
-    profits = int(budget_data[1])
+    #average = sum(profits)/len(profits)
 
-    average = sum(profits)/len(profits)
-
-    total =sum(profits)
+    #total =sum(profits)
     
-    max_profit = max(profits)
+    #max_profit = max(profits)
 
-    max_loss = min(profits)
+    #max_loss = min(profits)
+
+    #print(f"(Total: ${total})")
+    #print(f"(Average Change: ${average})")
+
+
 
 
 # Open CSV
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    header = next(cvsreader)
+    header=next(csvreader)
 
     for row in csvreader:
-        date.append(row[1])
-        Profits_Losses.append(row[2])
+        date.append(row[0])
+        Profits_Losses.append(int(row[1]))
+
+average = sum(Profits_Losses)/len(Profits_Losses) 
+print(average)
+ 
