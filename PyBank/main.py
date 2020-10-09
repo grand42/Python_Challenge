4307edced4de796c86594f34d6b33ce4332a27e9
Gretchen A <and01487@umn.edu>
2020-10-08 23:59:40 -0500

    Add functions

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,14 +5,30 @@ import csv
 # Set Path
 csvpath = os.path.join("..", "Resources","budget_data.csv")
 
+# Define Lists
+date = []
+Profits_Losses = []
+
 # Define Function
-def wins_and_losses(budget_data)
-date = str(budget_data)
-profits = int(budget_data[1])
+def profits_and_losses(budget_data):
+    
+    profits = int(budget_data[1])
+
+    average = sum(profits)/len(profits)
+
+    total =sum(profits)
+    
+    max_profit = max(profits)
+
+    max_loss = min(profits)
+
+
 # Open CSV
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(cvsreader)
 
-#
+    for row in csvreader:
+        date.append(row[1])
+        Profits_Losses.append(row[2])
